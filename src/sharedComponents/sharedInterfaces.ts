b31556157ec2ad62445f4325501cff6afa8d1424
Nutthapol Pota <nutthapolp@asergo.com>
2022-03-01 11:07:23 +0100

    added an apiResponse interface

--- src/sharedComponents/sharedInterfaces.ts
+++ src/sharedComponents/sharedInterfaces.ts
@@ -1,1 +1,6 @@
 //Add all interfaces for general use here (If an interface is used in both child & parent add it here)
+
+export interface apiResponse {
+    results: [] | null,
+    error: string | null
+}
