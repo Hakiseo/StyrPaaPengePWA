ae269b51d14bd2385cc7610bc8294dfde9514723
Nutthapol Pota <nutthapolp@asergo.com>
2022-03-09 22:32:54 +0100

    minor change - removed unused buttons

--- src/home.ts
+++ src/home.ts
@@ -1,7 +1,5 @@
 import {customElement, property} from "lit/decorators.js";
 import {html, LitElement, TemplateResult} from "lit";
-import {getAllParent} from "./api/parentApiRequests";
-import {getAllChildren} from "./api/childApiRequests";
 import "./sharedComponents/login"
 import "./sharedComponents/register"
 import {router} from "./index";
@@ -28,19 +26,6 @@ export class Home extends LitElement {
     }
 
     render(): TemplateResult {
-        return html `
-            <h1>AMAZinG Retards!!!</h1>
-            <button @click="${() => router.navigate("parent")}"> Go To Parent index </button>
-            <button @click="${() => router.navigate("parent/1")}"> Go To Parent 1 index </button>
-            <button @click="${() => router.navigate("child")}"> Go To Child index </button>
-            <button @click="${() => getAllChildren().then(r => console.log(r))}"> Get Children </button>
-            <button @click="${() => getAllParent().then(r => console.log(r))}"> Get Parents </button>
-            <hr>
-            ${this.renderHomeContent()}
-        `;
-    }
-
-    renderHomeContent() {
         if (this.showRegister) {
             return html `<register-page @showLogin="${() => this.showRegister = false}"></register-page>`
         }
