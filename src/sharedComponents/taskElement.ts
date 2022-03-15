fd6f96b5537be502e18a813e37972a2cb8914fea
Tomy <tabcaster@gmail.com>
2022-03-15 09:27:18 +0100

    -Ændret elementerne.

--- src/sharedComponents/taskElement.ts
+++ src/sharedComponents/taskElement.ts
@@ -9,65 +9,119 @@ export class TaskElement extends LitElement {
     @property({type: Boolean}) parentConfirmMode: boolean = false;
 
     static styles = [css`
-
-        :host{
-            display:block;
-            clear:both;
+        h4{
+            font-size: 15px;
         }
-
-        h5{
-            font-size:1.3rem;
-        }
-
         #img{
-            width:100px;
-            height:100px;
-            border-radius:15%;
-            margin-bottom:20px;
+            width:100%;
+            height:90px;
+            border-radius:12%;
+            margin-bottom:10px;
             background-size:cover;
             background-image:var(--image-url);
             background-color:var(--color, #ccc);
             float:left;
             overflow:hidden;
         }
-
         .taskElement{
-            color: white;
+            color: #E5E5E5;
             position: relative;
             background-color: #003865;
-            padding: 2rem;
+            padding: 0.8rem;
             text-align: center;
-            max-width: 100px;
-            min-width: 100px;
-            max-height:240px;
-            min-height:240px;
-            border-radius: 30px;
-            margin-bottom: 10px;
-            margin-top: 10px;
+            width: 90px;
+            height:180px;
+            border-radius: 25px;
+            margin-bottom: 7px;
+            margin-top: 7px;
         }
-
         .btn {
-            color: white;
-            padding: 7px 7px;
+            color: #FFFFFF;
+            padding: 5px 5px;
             text-align: center;
-            border-radius: 25px;
-            border: 2px solid #2a9fd8;
-            background: #2a9fd8;
+            border-radius: 20px;
+            border: 2px solid #000000;
+            background: #000000;
         }
-
-        .btn2 {
-            color: white;
-            padding: 7px 7px;
-            text-align: center;
-            border-radius: 25px;
-            border: 2px solid #2a9fd8;
-            background: #2a9fd8;
-            margin-top: 6px;
-            margin-bottom: 14px;
-        }
-  
         .btn:hover {
             color: white;
+        }
+        
+        @media screen and (min-width: 415px) {
+            h4{
+                font-size: 17px;
+            }
+            #img{
+                width:100px;
+                height:100px;
+                border-radius:15%;
+                margin-bottom:15px;
+                background-size:cover;
+                background-image:var(--image-url);
+                background-color:var(--color, #ccc);
+                float:left;
+                overflow:hidden;
+            }
+            .taskElement{
+                color: #E5E5E5;
+                position: relative;
+                background-color: #003865;
+                padding: 1rem;
+                text-align: center;
+                max-width: 100px;
+                min-width: 100px;
+                max-height:200px;
+                min-height:200px;
+                border-radius: 30px;
+                margin-bottom: 10px;
+                margin-top: 10px;
+            }
+            .btn {
+                color: #FFFFFF;
+                padding: 7px 7px;
+                text-align: center;
+                border-radius: 25px;
+                border: 2px solid #000000;
+                background: #000000;
+            }
+            .btn:hover {
+                color: white;
+            }
+        }
+        
+        @media screen and (min-width: 540px) {
+            #img{
+                width:130px;
+                height:130px;
+                border-radius:15%;
+                margin-bottom:20px;
+                background-size:cover;
+                background-image:var(--image-url);
+                background-color:var(--color, #ccc);
+                float:left;
+                overflow:hidden;
+            }
+            .taskElement{
+                color: #E5E5E5;
+                position: relative;
+                padding: 1rem;
+                text-align: center;
+                max-width: 130px;
+                min-width: 130px;
+                max-height:240px;
+                min-height:240px;
+                border-radius: 30px;
+                margin-bottom: 10px;
+                margin-top: 10px;
+            }
+            .btn {
+                color: #FFFFFF;
+                padding: 7px 7px;
+                text-align: center;
+                border-radius: 25px;
+                border: 2px solid #000000;
+                background: #000000;
+            }
         }
     `];
 
@@ -85,7 +139,7 @@ export class TaskElement extends LitElement {
             console.log(this.task)
             console.log(this.task.current_status)
             return html`
-                <article class="taskElement" style="${this.task.current_status == '1' ? `background-color:#006abf` : `background-color:#003865`}">
+                <article class="taskElement" style="${this.task.current_status == '1' ? `background-color:#FCA311` : `background-color:#14213D`}">
                     <div id="img" alt=${this.task.task_name}></div>
 
                     <h4>${this.task.current_status.length > 14 ? this.task.task_name.substring(0,14) : this.task.task_name.substring(0,11)}</h4>
--- src/sharedComponents/wishElement.ts
+++ src/sharedComponents/wishElement.ts
@@ -8,65 +8,119 @@ export class WishElement extends LitElement {
     @property({type: Boolean}) parentView: boolean = false;
 
     static styles = [css`
-
-        :host{
-            display:block;
-            clear:both;
+        h4{
+            font-size: 15px;
         }
-
-        h5{
-            font-size:1.3rem;
-        }
-
         #img{
-            width:100px;
-            height:100px;
-            border-radius:15%;
-            margin-bottom:20px;
+            width:100%;
+            height:90px;
+            border-radius:12%;
+            margin-bottom:10px;
             background-size:cover;
             background-image:var(--image-url);
             background-color:var(--color, #ccc);
             float:left;
             overflow:hidden;
         }
-
         .wishElement{
-            color: white;
+            color: #E5E5E5;
             position: relative;
             background-color: #003865;
-            padding: 2rem;
+            padding: 0.8rem;
             text-align: center;
-            max-width: 100px;
-            min-width: 100px;
-            max-height:240px;
-            min-height:240px;
-            border-radius: 30px;
-            margin-bottom: 10px;
-            margin-top: 10px;
+            width: 90px;
+            height:180px;
+            border-radius: 25px;
+            margin-bottom: 7px;
+            margin-top: 7px;
         }
-
         .btn {
-            color: white;
-            padding: 7px 7px;
+            color: #FFFFFF;
+            padding: 5px 5px;
             text-align: center;
-            border-radius: 25px;
-            border: 2px solid #2a9fd8;
-            background: #2a9fd8;
+            border-radius: 20px;
+            border: 2px solid #000000;
+            background: #000000;
         }
-
-        .btn2 {
-            color: white;
-            padding: 7px 7px;
-            text-align: center;
-            border-radius: 25px;
-            border: 2px solid #2a9fd8;
-            background: #2a9fd8;
-            margin-top: 6px;
-            margin-bottom: 14px;
-        }
-  
         .btn:hover {
             color: white;
+        }
+        
+        @media screen and (min-width: 415px) {
+            h4{
+                font-size: 17px;
+            }
+            #img{
+                width:100px;
+                height:100px;
+                border-radius:15%;
+                margin-bottom:15px;
+                background-size:cover;
+                background-image:var(--image-url);
+                background-color:var(--color, #ccc);
+                float:left;
+                overflow:hidden;
+            }
+            .wishElement{
+                color: #E5E5E5;
+                position: relative;
+                background-color: #003865;
+                padding: 1rem;
+                text-align: center;
+                max-width: 100px;
+                min-width: 100px;
+                max-height:200px;
+                min-height:200px;
+                border-radius: 30px;
+                margin-bottom: 10px;
+                margin-top: 10px;
+            }
+            .btn {
+                color: #FFFFFF;
+                padding: 7px 7px;
+                text-align: center;
+                border-radius: 25px;
+                border: 2px solid #000000;
+                background: #000000;
+            }
+            .btn:hover {
+                color: white;
+            }
+        }
+        
+        @media screen and (min-width: 540px) {
+            #img{
+                width:130px;
+                height:130px;
+                border-radius:15%;
+                margin-bottom:20px;
+                background-size:cover;
+                background-image:var(--image-url);
+                background-color:var(--color, #ccc);
+                float:left;
+                overflow:hidden;
+            }
+            .wishElement{
+                color: #E5E5E5;
+                position: relative;
+                padding: 1rem;
+                text-align: center;
+                max-width: 130px;
+                min-width: 130px;
+                max-height:240px;
+                min-height:240px;
+                border-radius: 30px;
+                margin-bottom: 10px;
+                margin-top: 10px;
+            }
+            .btn {
+                color: #FFFFFF;
+                padding: 7px 7px;
+                text-align: center;
+                border-radius: 25px;
+                border: 2px solid #000000;
+                background: #000000;
+            }
         }
     `];
 
@@ -104,7 +158,7 @@ export class WishElement extends LitElement {
             return html `Loading...`
         }else{
             return html`
-                <article class="wishElement" style="${this.wish.current_status == '1' ? `background-color:#006abf` : `background-color:#003865`}">
+                <article class="wishElement" style="${this.wish.current_status == '1' ? `background-color:#FCA311` : `background-color:#14213D`}">
                     <div id="img" alt=${this.wish.saving_name}></div>
                     <h4>${this.wish.current_status.length > 14 ? this.wish.saving_name.substring(0,14) : this.wish.saving_name.substring(0,11)}</h4>
                     ${this.parentView ? this.renderparent() : this.renderChild()}
