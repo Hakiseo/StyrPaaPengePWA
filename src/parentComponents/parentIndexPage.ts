8fa8cd506895f7dab83c7900f2b5189f09b4fcba
Nutthapol Pota <nutthapolp@asergo.com>
2022-03-16 11:09:06 +0100

    Space evenly flex container for junior cards

--- src/parentComponents/parentIndexPage.ts
+++ src/parentComponents/parentIndexPage.ts
@@ -139,7 +139,7 @@ export class ParentIndexPage extends LitElement {
         if (this.childrenData.length === 0) return;
         //Add loop here for all juniors and route on click to user id
         return html `
-            <div style="display: flex; flex-wrap: wrap">
+            <div style="display: flex; flex-wrap: wrap; justify-content: center">
                 ${this.childrenData.map((d: IChildData) => {
                     return html `
                         <junior-card .data="${d}" @click="${() => this.navigateToChild(d.id)}"></junior-card>
